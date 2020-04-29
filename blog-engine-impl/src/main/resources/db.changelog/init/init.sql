618f48b2a7e4b1003eaed484095af3884030da0c
Alexey Vasin <aevasin@vtb.ru>
2020-04-29 10:06:38 +0300

    make universal access for all user

--- blog-engine-impl/src/main/resources/db.changelog/init/init.sql
+++ blog-engine-impl/src/main/resources/db.changelog/init/init.sql
@@ -7,3 +7,10 @@ CREATE DATABASE sklbx;
 -- create schema
 CREATE SCHEMA be;
 
+CREATE USER be_user WITH password 'be_user';
+
+ALTER USER be_user WITH SUPERUSER;
+
+GRANT USAGE ON SCHEMA be TO be_user;
+
+ALTER SCHEMA be OWNER TO be_user;
